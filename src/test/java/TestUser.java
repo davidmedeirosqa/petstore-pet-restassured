9514cba69ab0b68c653d5cf535e8783d0e7b39b4
David Medeiros <davidmedeiros94@hotmail.com>
2025-02-21 10:05:12 -0300

    instalado dependências e criado a parte inicial

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,30 @@
+// 1- Nome do pacote
+
+// 2- Bibliotecas
+import org.junit.jupiter.api.Test;
+
+// 3- Classe
+public class TestUser {
+
+    // 3.1- Atributos
+    static String ct = "application/json"; // content-type
+    static String uriPet = "https://petstore.swagger.io/v2/pet"; // Base URL + Endpoint (user)
+
+    // 3.2 - Funções e métodos
+    // 3.2.1 - Funções e métodos comuns/úteis
+
+    // Função de leitura do JSON
+    public static String readFileJson(string fileJson) {
+        // TODO: Completar a leitura do arquivo
+        // return new String(Files);
+        return null;
+    }
+
+    // 3.2.2 - Métodos de testes
+    @Test
+    public void testPostPet() {
+        // TODO: Criar um método Post
+        // Carregar os dados do arquivo JSON do pet
+        // String jsonBody =
+    }
+}
