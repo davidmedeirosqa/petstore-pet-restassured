97441520aa3f693902610b2ed7e0bb387edf2375
David Medeiros <davidmedeiros94@hotmail.com>
2025-02-26 08:10:23 -0300

    tratamento de erros e teste pelo rest-assured

--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,7 +1,16 @@
 // 1- Nome do pacote
 
 // 2- Bibliotecas
+import io.restassured.response.Response; // Classe resposta do Rest-assured
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given; // Função given
+import static org.hamcrest.Matchers.*; // Classe de comparadores do Hamcrest
 
 // 3- Classe
 public class TestUser {
@@ -14,17 +23,37 @@ public class TestUser {
     // 3.2.1 - Funções e métodos comuns/úteis
 
     // Função de leitura do JSON
-    public static String readFileJson(string fileJson) {
-        // TODO: Completar a leitura do arquivo
-        // return new String(Files);
-        return null;
+    public static String readFileJson(String fileJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileJson)));
     }
 
     // 3.2.2 - Métodos de testes
     @Test
-    public void testPostPet() {
-        // TODO: Criar um método Post
+    public void testPostPet() throws IOException {
+
+        // Configura
         // Carregar os dados do arquivo JSON do pet
-        // String jsonBody =
+        String jsonBody = readFileJson("src/test/resources/json/pet1.json");
+        int petId = 118650001; // Código esperado do pet
+
+        // Teste pelo Rest-assured
+        given()                                          // Dado que
+            .contentType(ct)                             // Tipo de conteúdo
+            .log().all()                                 // Mostre tudo na ida
+            .body(jsonBody)                              // Envie o corpo da requisição
+
+        // Executa
+        .when()                                          // Quando
+            .post(uriPet)                                // Chamamos o Endpoint fazendo POST
+
+        // Valida 
+        .then()                                          // Então
+            .log().all()                                 // Mostre tudo na volta
+            .statusCode(200)          // Verificar código 200
+            .body("name", is("Bob"))                // Verificar se o nome é "Bob"
+            .body("id", is(petId))                  // Verificar se o id é "118650001"(Variável petId)
+            .body("category.name", is("cachorro"))  // Verificar se o nome da categoria é "cachorro"
+            .body("tags[0].name", is("vacinado"))   // Verificar se é "vacinado"
+        ; // Fechamento do Given
     }
 }
